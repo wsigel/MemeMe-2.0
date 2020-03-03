1be01d0bd69e6ca820a3535d05f8d6d978471dca
Wolfgang Sigel <wsigel@web.de>
2020-03-03 20:12:07 +0100

    Add collection view

---
+++ MemeMe 2.0/Controllers/SentMemeCollectionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SentMemeCollectionViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Wolfgang Sigel on 03.03.20.
+//  Copyright © 2020 Wolfgang Sigel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeCollectionViewController: UICollectionViewController {
+    
+    
+    
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SentMemeCell", for: indexPath) as! SentMemeCollectionViewCell
+        let sentMeme = self.memes[indexPath.row]
+        //cell.SentMemeImageView.image = sentMeme.memedImage
+        cell.SentMemeImageView.backgroundColor = UIColor.blue
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+}
---
+++ MemeMe 2.0/Views/SentMemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemeCollectionViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Wolfgang Sigel on 03.03.20.
+//  Copyright © 2020 Wolfgang Sigel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var SentMemeImageView: UIImageView!
+}
